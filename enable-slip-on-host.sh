8f67c61c793c8fc6623f19e5ee3f65a781b9cb20
Martin Unzner <martin.u@posteo.de>
2022-01-28 07:20:32 +0100

    bring SLIP interface up on initialization

--- enable-slip-on-host.sh
+++ enable-slip-on-host.sh
@@ -6,7 +6,7 @@ set -o nounset
 PORT=$1
 slattach -p slip -L -s 38400 "$PORT" &
 while ! ifconfig -a | grep -q sl0; do sleep 1; done
-ifconfig sl0 192.168.1.9 pointopoint 192.168.1.10 netmask 255.255.255.0 mtu 1024
+ifconfig sl0 192.168.1.9 pointopoint 192.168.1.10 netmask 255.255.255.0 mtu 1024 up
 ifconfig -a
 route add 192.168.1.10 sl0
 route -n
