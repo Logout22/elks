c4e8ef8bd6653d823bb24574ae82de8492f10781
Martin Unzner <martin.u@posteo.de>
2023-12-09 10:20:28 +0100

    throttle speed to reduce packet loss

--- enable-slip-on-host.sh
+++ enable-slip-on-host.sh
@@ -4,7 +4,7 @@ set -o nounset
 
 # /dev/pts/<ID> for qemu
 PORT=$1
-slattach -p slip -L -s 38400 "$PORT" &
+slattach -p slip -L -s 9600 "$PORT" &
 while ! ifconfig -a | grep -q sl0; do sleep 1; done
 ifconfig sl0 192.168.1.9 pointopoint 192.168.1.10 netmask 255.255.255.0 mtu 1024 up
 ifconfig -a
